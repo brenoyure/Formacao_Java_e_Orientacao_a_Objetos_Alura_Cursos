5d78b4444b4b1e79f63776162b1d3bd14a6547b6
brenoyure <byurifb@gmail.com>
2022-06-29 23:51:07 -0300

    desvendando a classe String

---
+++ byteBank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,19 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+
+        // int a = 3;
+        String nome = "Alura"; // object literal
+        // String outro = new String();
+
+        nome.replace('A', 'a');
+
+        System.out.println(nome);
+
+        
+
+    }
+
+}
