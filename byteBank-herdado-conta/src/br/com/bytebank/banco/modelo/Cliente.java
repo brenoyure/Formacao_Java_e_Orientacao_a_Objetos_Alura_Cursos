2e2c24450c2e6a10838da1d15107d13669c9c6eb
brenoyure <byurifb@gmail.com>
2022-07-13 23:48:58 -0300

    g

--- byteBank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ byteBank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -4,40 +4,59 @@ package br.com.bytebank.banco.modelo;
  * 
  * Classe que representa um cliente no byteBank.
  * 
- * @author Breno
- * @version 0.1
- *
+ * @author Breno Yuri
+ * @version 0.2
+ * @implNote toString() method added at line 43.
+ * @implNote adicionados construtores (vazio, nome, completo) a partir da linha
+ *           19.
  */
-
 
 public class Cliente {
 
-    private String nome;
-    private String cpf;
-    private String profissao;
+	private String nome;
+	private String cpf;
+	private String profissao;
 
-    public String getNome() {
-        return nome;
-    }
+	public Cliente() {
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	}
 
-    protected String getCpf() {
-        return cpf;
-    }
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
 
-    void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	public Cliente(String nome, String cpf, String profissao) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.profissao = profissao;
+	}
 
-    String getProfissao() {
-        return profissao;
-    }
+	public String getNome() {
+		return this.nome;
+	}
 
-    void setProfissao(String profissao) {
-        this.profissao = profissao;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	protected String getCpf() {
+		return cpf;
+	}
+
+	void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	private String getProfissao() {
+		return profissao;
+	}
+
+	protected void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + this.getNome();
+	}
 }
--- byteBank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ byteBank-herdado-conta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -1,16 +1,20 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+
 import br.com.bytebank.banco.modelo.*;
 
 public class Teste {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ArrayList lista = new ArrayList<Conta>();
-        //ArrayList lista1 = new ArrayList<Conta>();
+        // Generics
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+        // ArrayList lista1 = new ArrayList<Conta>();
 
         Conta cc = new ContaCorrente(22, 11);
+        // Cliente cliente = new Cliente("Breno Yuri");
+        // lista.add(cliente);
         Conta cc2 = new ContaCorrente(22, 22);
         Conta cc3 = new ContaCorrente(33, 311);
         Conta cc4 = new ContaCorrente(33, 322);
@@ -19,31 +23,28 @@ public class Teste {
         lista.add(cc2);
         lista.add(cc3);
         lista.add(cc4);
-
+        
         System.out.println("Tamanho: " + lista.size());
-        Conta ref = (Conta)lista.get(0);
+        
+        Conta ref = lista.get(0);
+        
+        System.out.println(lista.get(0).toString());
 
         System.out.println(ref.getNumeroConta());
 
         lista.remove(0);
 
         System.out.println("Tamanho: " + lista.size());
-        
-        for(int i = 0; i < lista.size(); i++) {
+
+        for (int i = 0; i < lista.size(); i++) {
             System.out.println(lista.get(i));
         }
 
         System.out.println("=======");
 
-        for(Object o : lista) {
-            System.out.println(o);
+        for (Conta conta : lista) {
+            System.out.println(conta);
         }
-
-
-
-
-
-
 
     }
 
