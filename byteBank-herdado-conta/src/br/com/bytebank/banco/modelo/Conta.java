32a9d45aea40f7af90755f6303fc6f838fca6290
brenoyure <byurifb@gmail.com>
2022-07-15 14:38:58 -0300

    ehIgual()

--- byteBank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ byteBank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -11,102 +11,112 @@ package br.com.bytebank.banco.modelo;
 
 public abstract class Conta {
 
-    protected double saldo;
-    private int agencia;
-    private int numeroConta;
-    private Cliente titular;
-    private static int total;
+	protected double saldo;
+	private int agencia;
+	private int numeroConta;
+	private Cliente titular;
+	private static int total;
 
-    /**
-     * 
-     * Construtor para inicializar o objeto Conta.
-     * A partir dos parâmentros agencia e número de conta
-     * 
-     * @param agencia
-     * @param numeroConta
-     * 
-     */
+	/**
+	 * 
+	 * Construtor para inicializar o objeto Conta. A partir dos parâmentros agencia
+	 * e número de conta
+	 * 
+	 * @param agencia
+	 * @param numeroConta
+	 * 
+	 */
 
-    public Conta(int agencia, int numeroConta) {
-        Conta.total++;
-        // System.out.println("O total de contas é " + Conta.total);
-        this.agencia = agencia;
-        this.numeroConta = numeroConta;
-        // System.out.println("Estou criando uma conta. - construtor padrão do Java");
-    }
+	public Conta(int agencia, int numeroConta) {
+		Conta.total++;
+		// System.out.println("O total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numeroConta = numeroConta;
+		// System.out.println("Estou criando uma conta. - construtor padrão do Java");
+	}
 
-    /**
-     * Método abstrato deposita, que recebe um double valorDeposito como parâmetro.
-     * 
-     * @param valorDeposito
-     */
+	/**
+	 * Método abstrato deposita, que recebe um double valorDeposito como parâmetro.
+	 * 
+	 * @param valorDeposito
+	 */
 
-    public abstract void deposita(double valorDeposito);
+	public abstract void deposita(double valorDeposito);
 
-    /**
-     * Método saca que recebe um double, que será subtraído do saldo,
-     * caso o saldo seja igual ou maior que o valor do saque.
-     * Saca, lançará uma exception do tipo Saldo Insuficiente caso o contrário.
-     * 
-     * @param valorSaque
-     * @throws SaldoInsuficienteException
-     */
-    public void saca(double valorSaque) throws SaldoInsuficienteException {
-        if (this.saldo < valorSaque) {
-            throw new SaldoInsuficienteException("Saldo: R$" + this.saldo + ", Valor do Saque: R$" + valorSaque);
-        }
-        this.saldo -= valorSaque;
-    }
+	/**
+	 * Método saca que recebe um double, que será subtraído do saldo, caso o saldo
+	 * seja igual ou maior que o valor do saque. Saca, lançará uma exception do tipo
+	 * Saldo Insuficiente caso o contrário.
+	 * 
+	 * @param valorSaque
+	 * @throws SaldoInsuficienteException
+	 */
+	public void saca(double valorSaque) throws SaldoInsuficienteException {
+		if (this.saldo < valorSaque) {
+			throw new SaldoInsuficienteException("Saldo: R$" + this.saldo + ", Valor do Saque: R$" + valorSaque);
+		}
+		this.saldo -= valorSaque;
+	}
 
-    public void transfere(Conta destino, double valorTransferencia) throws SaldoInsuficienteException {
-        this.saca(valorTransferencia);
-        destino.deposita(valorTransferencia);
-    }
+	public void transfere(Conta destino, double valorTransferencia) throws SaldoInsuficienteException {
+		this.saca(valorTransferencia);
+		destino.deposita(valorTransferencia);
+	}
 
-    public double getSaldo() {
-        return this.saldo;
-    }
+	public double getSaldo() {
+		return this.saldo;
+	}
 
-    public int getNumeroConta() {
-        return this.numeroConta;
-    }
+	public int getNumeroConta() {
+		return this.numeroConta;
+	}
 
-    void setNumeroConta(int novoNumeroConta) {
-        if (novoNumeroConta <= 0) {
-            System.out.println("Não é possível definir valor menor ou igual a zero");
-            return;
-        }
-        this.numeroConta = novoNumeroConta;
-    }
+	void setNumeroConta(int novoNumeroConta) {
+		if (novoNumeroConta <= 0) {
+			System.out.println("Não é possível definir valor menor ou igual a zero");
+			return;
+		}
+		this.numeroConta = novoNumeroConta;
+	}
 
-    public int getAgencia() {
-        return this.agencia;
-    }
+	public int getAgencia() {
+		return this.agencia;
+	}
 
-    void setAgencia(int numeroAgencia) {
-        if (agencia <= 0) {
-            System.out.println("Não pode valor menor ou igual a zero");
-            return;
-        }
+	void setAgencia(int numeroAgencia) {
+		if (agencia <= 0) {
+			System.out.println("Não pode valor menor ou igual a zero");
+			return;
+		}
 
-        this.agencia = numeroAgencia;
-    }
+		this.agencia = numeroAgencia;
+	}
 
-    void setTitular(Cliente titular) {
-        this.titular = titular;
-    }
+	void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
 
-    protected Cliente getTitular() {
-        return this.titular;
-    }
+	protected Cliente getTitular() {
+		return this.titular;
+	}
 
-    protected static int getTotal() {
-        return Conta.total;
-    }
+	protected static int getTotal() {
+		return Conta.total;
+	}
 
-    @Override
-    public String toString() {
-        return "Número: " + this.getNumeroConta() + ", Agência: " + this.getAgencia();
-    }
+	public boolean ehIgual(Conta outraConta) {
+		if (this.getAgencia() != outraConta.getAgencia()) {
+			return false;
+		}
+		if (this.getNumeroConta() != outraConta.getNumeroConta()) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Número: " + this.getNumeroConta() + ", Agência: " + this.getAgencia();
+	}
 
 }
--- byteBank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayListEquals.java
+++ byteBank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayListEquals.java
@@ -7,22 +7,35 @@ public class TesteArrayListEquals {
 
 	public static void main(String[] args) {
 
-		ArrayList<Conta> lista = new ArrayList<>();
-
-		Conta cc = new ContaCorrente(22, 11);
-		lista.add(cc);
-
+		Conta cc1 = new ContaCorrente(22, 22);
 		Conta cc2 = new ContaCorrente(22, 22);
-		lista.add(cc2);
 		
-		Conta cc3 = new ContaCorrente(22, 22);
+		boolean igual = cc1.ehIgual(cc2);
+		System.out.println(igual);
 		
-		boolean existe = lista.contains(cc3);
-		System.out.println(existe);
 		
-		for (Conta conta : lista) {
-			System.out.println(conta);
-		}
+//		ArrayList<Conta> lista = new ArrayList<>();
+//
+//		Conta cc = new ContaCorrente(22, 11);
+//		lista.add(cc);
+//
+//		Conta cc2 = new ContaCorrente(22, 22);
+//		lista.add(cc2);
+//		
+//		Conta cc3 = new ContaCorrente(22, 22);
+//		
+//		boolean existe = lista.contains(cc3);
+//		System.out.println("Já existe? " + existe);
+//		
+//		for(Conta conta: lista) {
+//			if(conta == cc3) {
+//				System.out.println("Ref já existe.");
+//			}
+//		}
+//		
+//		for (Conta conta : lista) {
+//			System.out.println(conta);
+//		}
 
 	}
 
