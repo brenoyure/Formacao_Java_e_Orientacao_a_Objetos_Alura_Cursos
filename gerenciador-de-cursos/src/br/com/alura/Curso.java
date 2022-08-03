0e45590e7478b606fc91a0ef622f36ab2e9be3e6
brenoyure <byurifb@gmail.com>
2022-08-03 19:05:38 -0300

    criando a classe Curso

---
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -0,0 +1,39 @@
+package br.com.alura;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<>();
+
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public String getInstrutor() {
+		return this.instrutor;
+	}
+
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Curso [nome=" + nome + ", instrutor=" + instrutor + ", aulas=" + aulas + "]";
+//	}
+
+}
---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,20 @@
+package br.com.alura;
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+
+		Curso colecoesJava = new Curso("Dominando as Coleções", "Paulo Silveira");
+	
+		colecoesJava.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		colecoesJava.adiciona(new Aula("Criando uma Aula", 20));
+		colecoesJava.adiciona(new Aula("Modeloando com Coleções", 24));
+		
+		System.out.println(colecoesJava.getAulas());
+		
+		
+		
+		
+	}
+
+}
